d2cc61cff88c21f96ef2710c76555bd1ac0a80c9
spelillo <spelillo11@gmail.com>
2026-04-28 22:03:08 -0400

    Update mlb_statcast_ingest.R

--- scripts/mlb_statcast_ingest.R
+++ scripts/mlb_statcast_ingest.R
@@ -17,7 +17,8 @@ if (Sys.getenv("GCP_AUTH_JSON") != "") {
 # --- 3. DATES (The "Double Definition") ---
 # We define BOTH names so the loop cannot fail regardless of which one it calls
 # Instead of 2023-2026, just get the last 2 weeks of data to stay current
-full_scrape_dates <- seq(Sys.Date() - 14, Sys.Date(), by = "4 days")date_chunks <- full_scrape_dates 
+full_scrape_dates <- seq(Sys.Date() - 14, Sys.Date(), by = "4 days")
+date_chunks <- full_scrape_dates
 
 message("Dates initialized. Starting loop...")
 
